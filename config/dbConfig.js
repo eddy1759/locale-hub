894d40c972fcdb70ae7721395b97346aaa035102
Edet Asuquo <eddy1759@gmail.com>
2023-08-04 18:28:22 +0100

    refactor: refactoring the codes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,14 +1,13 @@
 const { Sequelize } = require('sequelize');
 const CONFIG = require('./config');
-const { errorLogger, infoLogger } = require('../middleware/logger');
 
-const sequelize = new Sequelize( CONFIG.url, {
+const sequelize = new Sequelize(CONFIG.url, {
 	dialect: 'postgres',
 	dialectOptions: {
 		ssl: {
-			rejectUnauthorized: false
+			rejectUnauthorized: false,
 		},
 	},
 });
 
-module.exports = dbConnection;
+module.exports = sequelize;
