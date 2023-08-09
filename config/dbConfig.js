6f6779031a05da0f181e1e8b2c0a043541f13990
Edet Asuquo <eddy1759@gmail.com>
2023-08-09 01:35:26 +0100

    chore: configuration added

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
 const CONFIG = require('./config');
+const { infoLogger, errorLogger } = require('../src/utils/logger');
 
 const sequelize = new Sequelize(CONFIG.url, {
 	dialect: 'postgres',
@@ -10,4 +11,22 @@ const sequelize = new Sequelize(CONFIG.url, {
 	},
 });
 
-module.exports = sequelize;
+async function connectToDatabase() {
+	try {
+		await sequelize.authenticate();
+		infoLogger.info('Connection has been established successfully.');
+	} catch (error) {
+		errorLogger.error('Unable to connect to the database:', error);
+	}
+}
+
+async function syncTable() {
+	try {
+		await sequelize.sync({ force: false });
+		infoLogger.info('Database & tables synced');
+	} catch (error) {
+		errorLogger.error('Unable to sync database & tables:', error);
+	}
+}
+
+module.exports = { connectToDatabase, syncTable };
